ef8c8568bfc2be7f617f1cf4b4e590d438427d6e
n <nnizovkin>
2021-10-07 19:52:16 +0300

    Седьмая задача

---
+++ src/Euler7.hs
@@ -0,0 +1,13 @@
+module Euler7(findKthPrime) where
+import Euler3 
+
+--https://www.mccme.ru/free-books/dubna/balazard.pdf
+
+findKthPrime :: Integer -> Integer
+findKthPrime k =
+  last(findKthPrimeR k [2])
+findKthPrimeR :: Integer -> [Integer] -> [Integer]
+findKthPrimeR 1 l = l
+findKthPrimeR k l =
+  findKthPrimeR (k - 1) (l ++ [nextPrime (last l  + 1) l])  
+ 
