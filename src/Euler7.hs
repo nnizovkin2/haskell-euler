50310d7f99cb58069f6100d79b2892fd4f66e806
n <nnizovkin>
2021-10-07 19:54:56 +0300

    Синхронизация с удалённым репозиторием

--- src/Euler7.hs
+++ src/Euler7.hs
@@ -1,5 +1,5 @@
 module Euler7(findKthPrime) where
-import Euler3 
+import Euler3
 
 findKthPrime :: Integer -> Integer
 findKthPrime k =
@@ -7,5 +7,4 @@ findKthPrime k =
 findKthPrimeR :: Integer -> [Integer] -> [Integer]
 findKthPrimeR 1 l = l
 findKthPrimeR k l =
-  findKthPrimeR (k - 1) (l ++ [nextPrime (last l  + 1) l])  
- 
+  findKthPrimeR (k - 1) (l ++ [nextPrime (last l  + 1) l])
