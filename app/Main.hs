15d027d62058e60bc13945ac848a49c22ea65ba6
n <nnizovkin>
2021-10-28 22:19:38 +0300

    Пятьдесят девятая задача

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,14 @@
 module Main where
 
-import Euler60(ans)
+import Euler59(decrypt, dec, text)
+import Data.Bits(xor, (.&.))
+import Data.Char (chr, ord, toUpper)
       
 main:: IO()
-main = do 
-         print ans
+main = do
+         cipher<-readFile "p059_cipher.txt"
+         corpus<-readFile "words.txt"
+         print(decrypt cipher corpus)
 
 -- content <- readFile (args !! 0)
 -- linesOfFiles <- lines content
